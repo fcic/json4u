758231fe169a6f2877935a5bd174aa07a6be2784
fcic <fcic@qq.com>
2025-09-19 15:32:10 +0800

    Update page.tsx

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -4,6 +4,8 @@ import MainPanel from "@/containers/editor/panels/MainPanel";
 import SideNav from "@/containers/editor/sidenav";
 import { PricingOverlay } from "@/containers/pricing";
 
+export const runtime = 'edge';
+
 export default async function Page() {
   return (
     <TooltipProvider delayDuration={0}>
